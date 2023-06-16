112d532f35e36b33f2cdf7535bd581afc0a7d503
André Kuljis <zarkokuljis.zk@gmail.com>
2023-06-16 12:49:46 -0400

    updated Pad's timer

--- src/components/Pad.vue
+++ src/components/Pad.vue
@@ -2,7 +2,16 @@
 import LogoutIcon from "./icons/IconLogout.vue";
 import { url } from "@/resources/utils";
 
-import { isWithinInterval, isSameDay } from 'date-fns'
+import {
+    isWithinInterval,
+    isSameDay,
+    formatDistanceStrict,
+    differenceInMinutes,
+    formatDuration,
+    intervalToDuration,
+formatDistance,
+} from 'date-fns'
+import { es } from 'date-fns/locale'
 
 // @ts-expect-error
 import { $vfm, VueFinalModal, ModalsContainer } from 'vue-final-modal'
@@ -16,9 +25,7 @@ const logout = () => {
 </script>
 
 <script lang="ts">
-
 const nowInfo = await fetch(url + "now").then(response => response.json());
-console.log(nowInfo)
 
 export default {
     components: { VueFinalModal, ModalsContainer },
@@ -133,7 +140,7 @@ export default {
     },
     created() {
         this.requestNow();
-        this.requestNowInterval = window.setInterval(this.requestNow, 2000);
+        this.requestNowInterval = window.setInterval(this.requestNow, 1000);
     },
     beforeUnmount() {
         clearInterval(this.requestNowInterval);
@@ -159,99 +166,90 @@ export default {
                     <span class="nickname" style="vertical-align: middle;">{{ primo.nick }}</span>
                     &nbsp;<LogoutIcon v-on:click="logout" class="icon"/>
                 </div>
-                <span
-                    v-if="ushift.pair.length > 1 && ushift.pair.some( (p: any) => p.mail == primo.mail)"
-                >
-                    Pareja: {{
-                        ushift.pair.filter( (p: any) => p.mail != primo.mail ).map( (p: any) => p.nick ).join(", ")
+                <span v-if="ushift.pair.length > 1 && ushift.pair.some( (p: any) => p.mail == primo.mail)" >
+                    Pareja: {{ ushift.pair.filter( (p: any) => p.mail != primo.mail ).map( (p: any) => p.nick ).join(", ") }}
+                </span>
+            </div>
+            <!-- Si hay turnos corriendo y aún no termina el bloque -->
+            <div v-if="rshift != null && now <= new Date(nshift.checkout)" class="timer">
+                <span class="title">Tiempo restante de turno:</span>
+                <span class="time">
+                    {{ 
+                        formatDistanceStrict(
+                            new Date(nshift.checkout),
+                            now, 
+                            {
+                                unit: differenceInMinutes(new Date(nshift.checkout), now) < 1 ? 'second' : 'minute',
+                                roundingMethod: 'ceil',
+                                locale: es,
+                            }
+                        )
                     }}
                 </span>
             </div>
-            <!-- Si hay turnos pendientes -->
-            <div
-                v-if="rshift != null"
-            >
-                <span>Tiempo restante de turno:</span>
-                <div class="upcoming_shift">
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(nshift.checkout) - +new Date(now))/(60 * 60 * 1000))
-                        }}</span><span>h</span>
-                    </span>
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(nshift.checkout) - +new Date(now))/(60 * 1000)) % 60
-                        }}</span><span>m</span>
-                    </span>
-                </div>
+            <!-- Si hay turnos corriendo y ya terminó el bloque -->
+            <div v-else-if="rshift != null" class="timer">
+                <span class="title">El turno terminó hace:</span>
+                <span class="time" style="color: var(--red)">
+                    {{ formatDistance(new Date(nshift.checkout), now, {locale: es}) }}
+                </span>
             </div>
             <!--Si no hay turnos corriendo y el siguiente turno ya empezó-->
-            <div
-                v-else-if="+new Date(nshift.checkin) <= +now"
-            >
-                <span>El turno empezó hace:</span>
-                <div class="upcoming_shift">
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(now) - +new Date(nshift.checkin))/(60 * 60 * 1000))
-                        }}</span><span>h</span>
-                    </span>
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(now) - +new Date(nshift.checkin))/(60 * 1000)) % 60
-                        }}</span><span>m</span>
-                    </span>
-                </div>
+            <div v-else-if="new Date(nshift.checkin) <= now" class="timer">
+                <span class="title">El turno empezó hace:</span>
+                <span class="time" style="color: var(--red)">
+                    {{ formatDistance(new Date(nshift.checkin), now, {locale: es}) }}
+                </span>
             </div>
             <!--Si no hay turnos corriendo y el siguiente aún no empieza y es hoy-->
-            <div
-                v-else-if="isSameDay(new Date(nshift.checkin), now)"
-            >
-                <span>Próximo turno en:</span>
-                <div class="upcoming_shift">
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(nshift.checkin) - +new Date(now))/(60 * 60 * 1000))
-                        }}</span><span>h</span>
-                    </span>
-                    <span>
-                        <span class="time">{{
-                            Math.floor((+new Date(nshift.checkin) - +new Date(now))/(60 * 1000)) % 60
-                        }}</span><span>m</span>
-                    </span>
-                </div>
+            <div v-else-if="isSameDay(new Date(nshift.checkin), now)" class="timer">
+                <span class="title">Próximo turno en:</span>
+                <span class="time">
+                    {{ 
+                        differenceInMinutes(new Date(nshift.checkin), now, {roundingMethod: 'ceil'}) > 60?
+                            formatDuration(
+                                intervalToDuration({
+                                    start: now,
+                                    end: new Date(nshift.checkin)
+                                }),
+                                {
+                                    format: ['hours', 'minutes'],
+                                    delimiter: ' y ',
+                                    locale: es,
+                                }
+                            ):
+                            formatDistanceStrict(
+                                new Date(nshift.checkin),
+                                now, 
+                                {
+                                    unit: differenceInMinutes(new Date(nshift.checkin), now) > 1? 'minute': 'second',
+                                    roundingMethod: 'ceil',
+                                    locale: es,
+                                }
+                            )
+                    }}
+                </span>
             </div>
             <!--Si no hay turnos corriendo y el siguiente aún no empieza y no es hoy-->
-            <div
-                v-else
-            >
-                <div style="display: grid;">
-                    <span style="text-align: center;">Próximo turno el:</span>
-                    <span class="time">
-                        {{ new Date(nshift.checkin).toLocaleString('es-ES', {weekday: 'long'}) }} {{ nshift.block }}
-                    </span>
-                </div>
+            <div v-else class="timer">
+                <span class="title">Próximo turno el:</span>
+                <span class="time">
+                    {{ new Date(nshift.checkin).toLocaleString('es-ES', {weekday: 'long'}) }} {{ nshift.block }}
+                </span>
             </div>
             <div>
-                <!--Si no hay turnos corriendo ni es tu turno-->
-                <button disabled class="button attendance_button"
-                    v-if="rshift == null && !primo.onshift"
-                >
-                    INICIAR<br>
-                    TURNO
-                </button>
-                <!--Si no hay turnos corriendo y es tu turno-->
+                <!--Si no hay turnos corriendo-->
                 <button class="button attendance_button"
-                    v-on:click="pushShift"
-                    v-else-if="rshift == null && primo.onshift"
+                    v-if="rshift == null"
+                    :disabled="!primo.onshift" 
                 >
                     INICIAR<br>
                     TURNO
                 </button>
                 <!--Si hay algún turno corriendo y el siguiente no es inmediato-->
                 <button class="button attendance_button" style="background-color: var(--red);"
-                    v-on:click="updateShiftSafety"
                     v-else-if="!primo.onshift || (rshift.block == ushift.block)"
+                    v-on:click="updateShiftSafety"
                 >
                     MARCAR<br>
                     SALIDA
@@ -318,13 +316,16 @@ export default {
     vertical-align: middle;
     cursor: pointer;
 }
-.upcoming_shift {
+.timer {
     display: flex;
-    justify-content: center;
-    gap: .5em;
+    flex-direction: column;
 }
-.time {
+.timer > .title {
+    text-align: center;
+}
+.timer > .time {
     font-size: 28px;
+    text-align: center;
 }
 .button {
     background-color: var(--green);
